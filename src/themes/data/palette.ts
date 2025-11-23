fab9b43e610d009c6e5a83ae5a42685f399652d3
juanjo <juanjo@aroztegi.com>
2025-11-23 12:19:52 +0100

    Implement theme color palette

---
+++ src/themes/data/palette.ts
@@ -0,0 +1,37 @@
+import { Colors } from "../../interface";
+
+export const palette: Colors = {
+  // Base colors
+  background: "#181818",
+  foreground: "#f4f4ff",
+  uiBackground: "#282828",
+  selection: "#484848",
+  cursor: "#ffdd33",
+
+  // Primary colors
+  yellow: "#ffdd33",
+  stringGreen: "#73c936",
+  constantGreen: "#95a99f",
+  lilac: "#9e95c7",
+  red: "#f43841",
+  orange: "#cc8c3c",
+
+  // Grays
+  gray: "#7a7076",
+  darkGray: "#52494e",
+  nickel: "#b6b6b6",
+  white: "#ffffff",
+  blue: "#96a6c8",
+
+  bracketForeground: "#bfc5bf",
+  lightForeground: "#e4e4ef",
+  namespaceForeground: "#ece9da",
+
+  transparent: "#00000000",
+  findMatchBg: "#f4f4ff",
+  findMatchFg: "#0a0a0a",
+  findHighlightBg: "#8b8989de",
+  findHighlightFg: "#000000",
+  hoverHighlight: "#63626231",
+  wordHighlightStrong: "#282828",
+};
